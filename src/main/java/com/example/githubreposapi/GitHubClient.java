3d3f705e1c551dbbf285b11ebca8a7ccbd2f0fe9
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-01-04 10:26:57 +0100

    refactor: update nullability annotations in GitHubClient methods

--- src/main/java/com/example/githubreposapi/GitHubClient.java
+++ src/main/java/com/example/githubreposapi/GitHubClient.java
@@ -1,9 +1,9 @@
 package com.example.githubreposapi;
 
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -23,21 +23,21 @@ public class GitHubClient {
         return List.of(repos);
     }
 
-    private GitHubRepository @Nullable [] fetchUserRepos(String username) {
+    private @Nullable GitHubRepository[] fetchUserRepos(String username) {
         return gitHubClient.get()
                 .uri("/users/{username}/repos", username)
                 .retrieve()
                 .body(GitHubRepository[].class);
     }
 
-    public List<GitHubBranch> getBranchesForRepo(GitHubRepository repo) {
+    public @Nonnull List<GitHubBranch> getBranchesForRepo(GitHubRepository repo) {
         GitHubBranch[] branches = fetchBranchesForRepo(repo);
 
         assert branches != null;
         return List.of(branches);
     }
 
-    private GitHubBranch[] fetchBranchesForRepo(GitHubRepository repo) {
+    private @Nullable GitHubBranch[] fetchBranchesForRepo(GitHubRepository repo) {
         return gitHubClient.get()
                 .uri("/repos/{owner}/{repo}/branches", repo.owner().login(), repo.name())
                 .retrieve()
