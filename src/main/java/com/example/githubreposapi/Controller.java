c1eef3dd789c57fd24ab5c05fde57b4d86e561fe
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-01-04 14:21:04 +0100

    refactor: remove unused Slf4j annotation from Controller class

--- src/main/java/com/example/githubreposapi/Controller.java
+++ src/main/java/com/example/githubreposapi/Controller.java
@@ -1,7 +1,6 @@
 package com.example.githubreposapi;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
-@Slf4j
 @RestController
 @RequestMapping("/repos")
 @RequiredArgsConstructor
